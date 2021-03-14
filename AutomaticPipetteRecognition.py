34dfe2e73b9ac417f1e466891936960d9625c7ef
TvdrBurgt <t.h.j.vanderburgt@gmail.com>
2021-03-14 12:53:31 +0100

    Optimized pipette recognition does not include drawing a canvas anymore

--- AutomaticPipetteRecognition.py
+++ AutomaticPipetteRecognition.py
@@ -7,13 +7,15 @@ Created on Mon Feb 22 12:17:08 2021
 
 import os
 import numpy as np
-from skimage import io, filters, feature, transform, draw
-import matplotlib.pyplot as plt
+from skimage import io
 
 
 # =============================================================================
 # Automatic Pipette Recognition
 # =============================================================================
+
+# load function from other file
+from ModifiedPipetteRecognition import detectPipettetip
 
 
 class DetectPipetteTips:
@@ -21,237 +23,152 @@ class DetectPipetteTips:
     This class iterates through a folder containing .tif files and extracts the
     (x,y)-coordinates of pipette tips.
     """
-    def __init__(self,path):
-        self.folder = path
-        self.savename = "Translation space Boyden"
-        self.filenames = os.listdir(path)
-        self.filecount = -1
-        self.lastfile = len(self.filenames)
-        self.pipettetips = []                   # estimated tip coordinates
+    def __init__(self,path,plotfile=''):
+        self.folder = path              # folder containing the images
+        self.pipettetips = []           # list to save pipette tip coordinates
+        self.difficultimages = []       # list of images where algorithm fails
+        self.plotfile = plotfile        # this file will be plotted
+        self.upper_angle = 97.5         # only for first calibration
+        self.lower_angle = 82.5         # only for first calibration
+        self.pipettediameter = 16.5     # only for first calibration
         
         
-    def nextImage(self):
+    def iterator(self):
         """
-        This function calls the pipette recognition algorithm for the next .tif
-        file. It also checks if it has reached the last file in the folder.
+        This function iterates over all files in the specified folder unless a
+        filename is given, in this case it will open that file for plotting only
         """
-        self.filecount += 1
-        print("File number %d/%d" % (self.filecount+1,self.lastfile))
+        # extract filenames
+        filenames = os.listdir(self.folder)
+        num_files = len(filenames)
         
-        # check if we arrived at the last file
-        if self.filecount < self.lastfile:
-            filepath = self.folder + "\\" + str(self.filenames[self.filecount])
-            
-            # check if file is a .tif before opening
-            if filepath.endswith('.tif'):
-                self.I = io.imread(filepath)
-                # run pipette tip recognition algorithm
-                self.pipetteRecognition()
-            self.nextImage()
-                
+        # skip all images instead of the one you want to plot
+        if not self.plotfile:
+            self.plotflag = False
         else:
-            print("Last file in folder reached")
-            self.list2file()
+            filenames = [self.plotfile,'']
+            num_files = 1
+            self.plotflag = True
+        
+        # iterate over all files
+        for index,filename in enumerate(filenames):
+            
+            # read image
+            if filename.endswith('.tif'):
+                I = io.imread(self.folder + '\\' + filename)
+                print("\nFile number %d/%d" % (index+1,num_files))
+            else:
+                continue
+            
+            # pipette tip detection algorithm
+            x1, y1 = detectPipettetip(I,
+                                      self.upper_angle,
+                                      self.lower_angle,
+                                      self.pipettediameter,
+                                      blursize=15,
+                                      angle_range=10,
+                                      num_angles=1000,
+                                      num_peaks=8,
+                                      plotflag=self.plotflag)
+            
+            # crop image
+            Icropped, xref, yref, faultylocalisation = self.cropImage(I, x1, y1)
+            
+            # assign (x,y)=(nan,nan) as coordinate if Icropped is fully outside I
+            if faultylocalisation:
+                self.difficultimages.append([filename])
+                self.pipettetips.append([filename, (np.nan, np.nan)])
+                continue
+            
+            # pipette tip detection algorithm
+            x2, y2 = detectPipettetip(Icropped,
+                                      self.upper_angle,
+                                      self.lower_angle,
+                                      self.pipettediameter,
+                                      blursize=4,
+                                      angle_range=10,
+                                      num_angles=5000,
+                                      num_peaks=6,
+                                      plotflag=self.plotflag)
+            
+            # adjusting x2 and y2 with the reference coordinates
+            x = xref + x2
+            y = yref + y2
+            
+            # saving pipette tip coordinates
+            print('Pipette tip detected @ (x,y) = (%f,%f)' % (x,y))
+            self.pipettetips.append([filename, (x, y)])
+            
     
-    def pipetteRecognition(self):
+    def cropImage(self,I,xpos,ypos,xsize=204,ysize=204):
         """
-        This function contains the pipette recognition algorithm itself. It
-        performs the image analysis steps and finds a maximum number of lines
-        [num_lines] to describe the pipette point. The intersection should be
-        the pipette tip.
+        This functions crops the input figure around the coordinates from the
+        first tip detection algorithm.
         """
-        num_lines = 7 # maximum number of lines to find
+        # round pipette coordinates to integers
+        xpos = np.round(xpos)
+        ypos = np.round(ypos)
         
-        print('I) Gaussian blurring...')
-        IB = filters.gaussian(self.I, 15)
+        # specify cropping region
+        left = int(xpos-xsize/2)
+        right = int(xpos+xsize/2)
+        up = int(ypos-ysize/2)
+        down = int(ypos+ysize/2)
         
-        print('II) Canny edge detection...')
-        BW = feature.canny(IB, sigma=10, low_threshold=0.9, high_threshold=0.7, use_quantiles=True)
+        # check if the cropping exceeds image boundaries
+        faultylocalisation = False
+        height = I.shape[0]
+        width = I.shape[1]
+        if left < 0:
+            left = 0
+            if right < 0:
+                right = xsize
+                faultylocalisation = True
+        if right > width-1:
+            right = width-1
+            if left > width-1:
+                left = width-1 - xsize
+                faultylocalisation = True
+        if up < 0:
+            up = 0
+            if down < 0:
+                down = ysize
+                faultylocalisation = True
+        if down > height-1:
+            down = height-1
+            if up > height-1:
+                up = height-1 - ysize
+                faultylocalisation = True
         
-        print('III) Calculating Hough transform...')
-        self.H, self.T, self.R = transform.hough_line(BW,np.linspace(5*np.pi/12,7*np.pi/12,120*100))
+        # crop image
+        Icropped = I[up:down,left:right]
         
-        print('IV) Finding most common lines from Hough transform...')
-        _, self.Tcommon, self.Rcommon = transform.hough_line_peaks(self.H,self.T,self.R,num_peaks=num_lines)
+        # reference point (x0,y0) is (0,0) in the cropped image
+        x0 = left
+        y0 = up
         
-        print('V) Filling canvas with most the %d most common lines...' % num_lines)
-        self.canvas = np.zeros((self.I.shape[0], self.I.shape[1]))
-        for r0, c0, r1, c1 in zip(*self.Houghlines2Imageboundary(self.Tcommon,self.Rcommon,self.I)):
-            rr, cc = draw.line(r0, c0, r1, c1)
-            self.canvas[rr, cc] += 1
-        self.canvas = filters.gaussian(self.canvas, 5)
-        
-        # extract pipette tip
-        ypos, xpos = np.where(self.canvas == np.max(self.canvas))
-        if len(xpos) != 1:
-            ypos = ypos[0]
-            xpos = xpos[0]
-        print('VI) Pipette tip located at pixel coordinate: (x,y) = (%d,%d).' % (xpos,ypos))
-        
-        # saving pipette tip coordinates
-        self.pipettetips.append([self.filenames[self.filecount], (xpos, ypos)])
-        
-        # plot result
-        # self.plotFigures()
-        
-        # go to next file
-        self.nextImage()
-        
-        
-    def Houghlines2Imageboundary(self,T,R,img):
-        """
-        This function extracts the intersection coordinates between extended Hough-
-        lines with the image boundary. The output are the rows and columns of the
-        image where the infinite Houghlines leave/enter the image.
-        """
-        # get width and height of image
-        height = img.shape[0]
-        width = img.shape[1]
-        
-        # divide angles into parallel and perpundicular to prevent exploding slopes
-        cutoff = np.abs(np.sin(T))
-        Tpar = T[np.where(cutoff >= np.sqrt(2)/2)]
-        Rpar = R[np.where(cutoff >= np.sqrt(2)/2)]
-        Tperp = T[np.where(cutoff < np.sqrt(2)/2)]
-        Rperp = R[np.where(cutoff < np.sqrt(2)/2)]
-        
-        # determine a and b in y=ax+b (parallel)
-        a = np.tan(np.pi/2+Tpar)
-        b = Rpar/np.sin(Tpar)
-        # determine c and d in x=cy+d (perpundicular)
-        c = np.tan(-Tperp)
-        d = Rperp/np.cos(Tperp)
-        
-        # prevent dividing by zero in the next step
-        a[a==0] = 1e-9
-        c[c==0] = 1e-9
-        
-        # determine crossings with the lines: y=0, x=0, y=height, x=width (parallel)
-        x0 = -b/a
-        y0 = b
-        xh = (height-b)/a
-        yw = a*width + b
-        # determine crossings with the lines: y=0, x=0, y=height, x=width (perpundicular)
-        x_0 = d
-        y_0 = -d/c
-        x_h = c*height + d
-        y_w = (width-d)/c
-        
-        # place crossings on the image edge (parallel)
-        x0[x0>width-1] = width-1
-        x0[x0<0] = 0
-        y0[y0>height-1] = height-1
-        y0[y0<0] = 0
-        xh[xh>width-1] = width-1
-        xh[xh<0] = 0
-        yw[yw>height-1] = height-1
-        yw[yw<0] = 0
-        # place crossings on the image edge (perpundicular)
-        x_0[x_0>width-1] = width-1
-        x_0[x_0<0] = 0
-        y_0[y_0>height-1] = height-1
-        y_0[y_0<0] = 0
-        x_h[x_h>width-1] = width-1
-        x_h[x_h<0] = 0
-        y_w[y_w>height-1] = height-1
-        y_w[y_w<0] = 0
-        
-        # match coordinates (parallel)
-        xleft = np.zeros((len(Tpar)))
-        yleft = np.zeros((len(Tpar)))
-        xright = np.zeros((len(Tpar)))
-        yright = np.zeros((len(Tpar)))
-        for i in range(len(Tpar)):
-            xleft[i] = np.min([x0[i],xh[i]])
-            xright[i] = np.max([x0[i],xh[i]])
-            if a[i] > 0:
-                yleft[i] = np.min([y0[i],yw[i]])
-                yright[i] = np.max([y0[i],yw[i]])
-            else:
-                yleft[i] = np.max([y0[i],yw[i]])
-                yright[i] = np.min([y0[i],yw[i]])
-        # match coordinates (perpundicular)
-        x_left = np.zeros((len(Tperp)))
-        y_left = np.zeros((len(Tperp)))
-        x_right = np.zeros((len(Tperp)))
-        y_right = np.zeros((len(Tperp)))
-        for i in range(len(Tperp)):
-            x_left[i] = np.min([x_0[i],x_h[i]])
-            x_right[i] = np.max([x_0[i],x_h[i]])
-            if c[i] > 0:
-                y_left[i] = np.min([y_0[i],y_w[i]])
-                y_right[i] = np.max([y_0[i],y_w[i]])
-            else:
-                y_left[i] = np.max([y_0[i],y_w[i]])
-                y_right[i] = np.min([y_0[i],y_w[i]])
+        return Icropped, x0, y0, faultylocalisation
                 
-        # let x and y be rows and columns (parallel)
-        c0 = np.around(xleft).astype(int)
-        r0 = np.around(yleft).astype(int)
-        c1 = np.around(xright).astype(int)
-        r1 = np.around(yright).astype(int)
-        # let x and y be rows and columns (perpundicular)
-        c_0 = np.around(x_left).astype(int)
-        r_0 = np.around(y_left).astype(int)
-        c_1 = np.around(x_right).astype(int)
-        r_1 = np.around(y_right).astype(int)
         
-        # append coordinates from parallel and perpundicular lines
-        C0 = np.append(c0,c_0)
-        R0 = np.append(r0,r_0)
-        CW = np.append(c1,c_1)
-        RH = np.append(r1,r_1)
-        
-        return R0, C0, RH, CW
-    
-    
-    def plotFigures(self):
-        xpos = self.pipettetips[-1][1]
-        ypos = self.pipettetips[-1][2]
-        
-        f1, axs1 = plt.subplots(1,3)
-        axs1[0].imshow(self.I, cmap='gray'); axs1[0].axis('image'); axs1[0].axis('off')
-        axs1[0].scatter(x=xpos, y=ypos, c='r', s=30)
-        axs1[0].annotate('(%d,%d)' % (xpos,ypos), (xpos+30, ypos-15), c='r')
-        axs1[1].imshow(self.canvas, cmap='gray'); axs1[1].axis('image'); axs1[1].axis('off')
-        axs1[2].imshow(np.log(1+self.H), extent=[np.rad2deg(self.T[0]), np.rad2deg(self.T[-1]), self.R[-1], self.R[0]], aspect='auto')
-        axs1[2].set_xlabel(r'$\theta$ (degree)')
-        axs1[2].set_ylabel(r'$\rho$ (pixels)')
-        
-        for i in range(0, len(self.Rcommon)):
-            axs1[2].scatter(x=[self.Tcommon[i]*180/np.pi], y=[self.Rcommon[i]], c='r', s=20)
-        
-        plt.pause(10)
-        input("Press enter to continue...")
-        plt.close()
-        
-    
-    def list2file(self):
-        # write final coordinates to a .csv file
-        with open(self.folder+'\\'+self.savename, 'w') as txtfile:
+    def save2file(self,savename):
+        """
+        This function writes the pipette tip coordinate list to a .csv file
+        """
+        with open(self.folder+'\\'+savename, 'w') as txtfile:
             txtfile.write("filename;x;y\n")
-            # for item in self.pipettetips:
-            #     txtfile.write("{};".format(item[0]))
-            #     try:
-            #         txtfile.write("%d;" % item[1][0])
-            #     except:
-            #         txtfile.write("%d;" % item[1])
-            #     try:
-            #         txtfile.write("%d\n" % item[2][0])
-            #     except:
-            #         txtfile.write("%d\n" % item[2])
             for item in self.pipettetips:
                 txtfile.write("{};".format(item[0]))
-                txtfile.write("%d;" % item[1][0])
-                txtfile.write("%d\n" % item[1][1])
-        print("Files saved in [path] with name {}".format(self.savename))
+                txtfile.write("%f;" % item[1][0])
+                txtfile.write("%f\n" % item[1][1])
+        print("\nFiles saved as: '{}'".format(savename))
+
 
 
 if __name__ == '__main__':
-    # fill in the complete path of the folder containing pipette tip images
-    path = r"C:\Users\tvdrb\Desktop\Thijs\Translation space"
+    path = r"C:\Users\tvdrb\Desktop\Thijs\XY grid"
+    savename = "XY grid modified algorithm"
     
-    pipettetips = DetectPipetteTips(path)
-    pipettetips.nextImage()
-    
+    imagefolder = DetectPipetteTips(path)
+    imagefolder.iterator()
+    imagefolder.save2file(savename)
     
--- PipetteRecognition.py
+++ PipetteRecognition.py
@@ -6,188 +6,136 @@ Created on Mon Feb 22 12:17:08 2021
 """
 
 import numpy as np
-from skimage import io, filters, feature, transform, draw
+from skimage import io, filters, feature, transform
 import matplotlib.pyplot as plt
 
 
 # =============================================================================
-# Pipette Recognition (based on Boyden et al.'s autopatcher IG)
+# Pipette Recognition
 # =============================================================================
 
-# suppress plots by choosing [False]
-plotflag = True
-
 # load tiff
-filepath = r"C:\Users\tvdrb\Desktop\Thijs\Translation space\focus 250 200 2.tif"
+filepath = r"C:\Users\tvdrb\Desktop\Thijs\XY grid\grid 150 -100 0.tif"
 I = io.imread(filepath)
 
-# Convert Tiff to 2D if Z-stack is the same
-# I = I[:,:,0]
+######################### parameter settings #########################
+# physical parameters
+upper_angle = 97.5  # guess upper angle of pipette (in degree)
+lower_angle = 82.5  # guess lower angle of pipette (in degree)
+diameter = 16.5     # pipette tip diameter (in pixels)
 
-####################### tip detection algorithm #######################
+######################################################################
 
-def Houghlines2Imageboundary(T,R,img):
+
+def plotCoord(I,angle1,dist1,angle2,dist2,xpos,ypos):
+    fig, axs = plt.subplots(1,2)
+    # subplot with Houghlines
+    axs[0].matshow(I, cmap='gray'); axs[0].axis('image')
+    (x1, y1) = dist1*np.array([np.cos(angle1), np.sin(angle1)])
+    (x2, y2) = dist2*np.array([np.cos(angle2), np.sin(angle2)])
+    axs[0].axline((x1 ,y1), slope=np.tan(angle1 + np.pi/2), c='r')
+    axs[0].axline((x2 ,y2), slope=np.tan(angle2 + np.pi/2), c='g')
+    # subplot with tip coordinates
+    axs[1].matshow(I, cmap='gray'); axs[1].axis('image')
+    axs[1].scatter(x=xpos, y=ypos, c='r', s=30)
+    axs[1].annotate('(%d,%d)' % (xpos,ypos), (xpos+30, ypos-15), c='r')
+    fig.show()
+    
+def plotBlurCannyHough(IB,BW,Tcommon1,Rcommon1,Tcommon2,Rcommon2):
+    fig, axs = plt.subplots(1,3)
+    # subplot with original image blurred
+    axs[0].matshow(IB, cmap='gray', aspect = 'auto'); axs[0].axis('image'); axs[0].axis('off')
+    for angle, dist in zip(Tcommon1,Rcommon1):
+        (x, y) = dist*np.array([np.cos(angle), np.sin(angle)])
+        axs[0].axline((x ,y), slope=np.tan(angle + np.pi/2), c='r')
+    for angle, dist in zip(Tcommon2,Rcommon2):
+        (x, y) = dist*np.array([np.cos(angle), np.sin(angle)])
+        axs[0].axline((x ,y), slope=np.tan(angle + np.pi/2), c='g')
+    # subplot with canny edge detection
+    axs[1].matshow(BW, cmap='gray', aspect = 'auto'); axs[1].axis('image'); axs[1].axis('off')
+    # subplot with Houghspace
+    left_theta = min(min(Tcommon1),min(Tcommon2))
+    right_theta = max(max(Tcommon1),max(Tcommon2))
+    H, T, R = transform.hough_line(BW,np.linspace(left_theta,right_theta,10000))
+    axs[2].imshow(np.log(1+H), extent=[np.rad2deg(T[0]), np.rad2deg(T[-1]), R[-1], R[0]], aspect='auto')
+    axs[2].set_xlabel(r'$\theta$ (degree)')
+    axs[2].set_ylabel(r'$\rho$ (pixels)')
+    for angle, dist in zip(Tcommon1,Rcommon1):
+        axs[2].scatter(x=[np.rad2deg(angle)], y=[dist], c='r', s=20)
+    for angle, dist in zip(Tcommon2,Rcommon2):
+        axs[2].scatter(x=[np.rad2deg(angle)], y=[dist], c='g', s=20)
+    fig.show()
+    
+
+def detectPipettetip(I, upper_angle, lower_angle, diameter, blursize=15, 
+                     angle_range=10, num_angles=5000, num_peaks=8, plotflag=True):
+    """ 
+    Tip detection algorithm 
+    software parameters:
+    blursize    = kernel size for gaussian blur
+    angle_range = angle search range (in degree)
+    num_angles  = number of sampling angles in Hough transform
+    num_peaks   = number of peaks to find in Hough space
+    plotflag    = if True it will generate figures
     """
-    This function extracts the intersection coordinates between extended Hough-
-    lines with the image boundary. The output are the rows and columns of the
-    image where the infinite Houghlines leave/enter the image.
-    """
-    # get width and height of image
-    height = img.shape[0]
-    width = img.shape[1]
     
-    # divide angles into parallel and perpundicular to prevent exploding slopes
-    cutoff = np.abs(np.sin(T))
-    Tpar = T[np.where(cutoff >= np.sqrt(2)/2)]
-    Rpar = R[np.where(cutoff >= np.sqrt(2)/2)]
-    Tperp = T[np.where(cutoff < np.sqrt(2)/2)]
-    Rperp = R[np.where(cutoff < np.sqrt(2)/2)]
+    # Gaussian blur
+    print('I)\t Gaussian blurring...')
+    IB = filters.gaussian(I, blursize)
     
-    # determine a and b in y=ax+b (parallel)
-    a = np.tan(np.pi/2+Tpar)
-    b = Rpar/np.sin(Tpar)
-    # determine c and d in x=cy+d (perpundicular)
-    c = np.tan(-Tperp)
-    d = Rperp/np.cos(Tperp)
+    # Canny edge detection
+    print('II)\t Canny edge detection...')
+    BW = feature.canny(IB, sigma=10, low_threshold=0.98, high_threshold=0.7, use_quantiles=True)
     
-    # prevent dividing by zero in the next step
-    a[a==0] = 1e-9
-    c[c==0] = 1e-9
+    # Double-sided Hough transform
+    print('III) Calculating Hough transform...')
+    if np.abs(upper_angle-lower_angle) < angle_range:
+        theta1 = upper_angle + np.linspace(angle_range/2, -np.abs(upper_angle-lower_angle)/2, num_angles)
+        theta2 = lower_angle + np.linspace(np.abs(upper_angle-lower_angle)/2, -angle_range/2, num_angles)
+    else:
+        theta1 = upper_angle + np.linspace(angle_range/2, -angle_range/2, num_angles)
+        theta2 = lower_angle + np.linspace(angle_range/2, -angle_range/2, num_angles)
+    # append theta's and transform to radians
+    theta = np.deg2rad(np.append(theta1,theta2))
+    # calculate Hough transform
+    H, T, R = transform.hough_line(BW,theta)
+    # split Hough transform in two because of two angles
+    H1, H2 = np.hsplit(H,2)
+    T1, T2 = np.hsplit(T,2)
     
-    # determine crossings with the lines: y=0, x=0, y=height, x=width (parallel)
-    x0 = -b/a
-    y0 = b
-    xh = (height-b)/a
-    yw = a*width + b
-    # determine crossings with the lines: y=0, x=0, y=height, x=width (perpundicular)
-    x_0 = d
-    y_0 = -d/c
-    x_h = c*height + d
-    y_w = (width-d)/c
+    # extract most common lines in the image from the double-sided Hough transform
+    print('IV)\t Finding most common lines from Hough transform...')
+    _, Tcommon1, Rcommon1 = transform.hough_line_peaks(H1,T1,R,num_peaks=num_peaks,threshold=0)
+    _, Tcommon2, Rcommon2 = transform.hough_line_peaks(H2,T2,R,num_peaks=num_peaks,threshold=0)
+    # find the average value so we end up with two lines
+    angle1 = np.mean(Tcommon1)
+    dist1 = np.mean(Rcommon1)
+    angle2 = np.mean(Tcommon2)
+    dist2 = np.mean(Rcommon2)
     
-    # place crossings on the image edge (parallel)
-    x0[x0>width-1] = width-1
-    x0[x0<0] = 0
-    y0[y0>height-1] = height-1
-    y0[y0<0] = 0
-    xh[xh>width-1] = width-1
-    xh[xh<0] = 0
-    yw[yw>height-1] = height-1
-    yw[yw<0] = 0
-    # place crossings on the image edge (perpundicular)
-    x_0[x_0>width-1] = width-1
-    x_0[x_0<0] = 0
-    y_0[y_0>height-1] = height-1
-    y_0[y_0<0] = 0
-    x_h[x_h>width-1] = width-1
-    x_h[x_h<0] = 0
-    y_w[y_w>height-1] = height-1
-    y_w[y_w<0] = 0
+    # find intersection between X1*cos(T1)+Y1*sin(T1)=R1 and X2*cos(T2)+Y2*sin(T2)=R2
+    print('V)\t Calculating preliminary pipette point...')
+    LHS = np.array([[np.cos(angle1), np.sin(angle1)], [np.cos(angle2), np.sin(angle2)]])
+    RHS = np.array([dist1, dist2])
+    xpos, ypos = np.linalg.solve(LHS, RHS)
     
-    # match coordinates (parallel)
-    xleft = np.zeros((len(Tpar)))
-    yleft = np.zeros((len(Tpar)))
-    xright = np.zeros((len(Tpar)))
-    yright = np.zeros((len(Tpar)))
-    for i in range(len(Tpar)):
-        xleft[i] = np.min([x0[i],xh[i]])
-        xright[i] = np.max([x0[i],xh[i]])
-        if a[i] > 0:
-            yleft[i] = np.min([y0[i],yw[i]])
-            yright[i] = np.max([y0[i],yw[i]])
-        else:
-            yleft[i] = np.max([y0[i],yw[i]])
-            yright[i] = np.min([y0[i],yw[i]])
-    # match coordinates (perpundicular)
-    x_left = np.zeros((len(Tperp)))
-    y_left = np.zeros((len(Tperp)))
-    x_right = np.zeros((len(Tperp)))
-    y_right = np.zeros((len(Tperp)))
-    for i in range(len(Tperp)):
-        x_left[i] = np.min([x_0[i],x_h[i]])
-        x_right[i] = np.max([x_0[i],x_h[i]])
-        if c[i] > 0:
-            y_left[i] = np.min([y_0[i],y_w[i]])
-            y_right[i] = np.max([y_0[i],y_w[i]])
-        else:
-            y_left[i] = np.max([y_0[i],y_w[i]])
-            y_right[i] = np.min([y_0[i],y_w[i]])
-            
-    # let x and y be rows and columns (parallel)
-    c0 = np.around(xleft).astype(int)
-    r0 = np.around(yleft).astype(int)
-    c1 = np.around(xright).astype(int)
-    r1 = np.around(yright).astype(int)
-    # let x and y be rows and columns (perpundicular)
-    c_0 = np.around(x_left).astype(int)
-    r_0 = np.around(y_left).astype(int)
-    c_1 = np.around(x_right).astype(int)
-    r_1 = np.around(y_right).astype(int)
+    # account for xposition overestimation bias
+    print('VI)\t Correcting for pipette diameter...')
+    deltax = (diameter/2)/np.sin(np.abs(angle1-angle2)/2)
+    deltay = (diameter/2)*np.cos((angle1+angle2)/2)
+    xpos = xpos - deltax
+    ypos = ypos + deltay
+    print(deltay)
     
-    # append coordinates from parallel and perpundicular lines
-    C0 = np.append(c0,c_0)
-    R0 = np.append(r0,r_0)
-    CW = np.append(c1,c_1)
-    RH = np.append(r1,r_1)
+    # plot figures if plotflag is True
+    if plotflag:
+        plotCoord(I,angle1,dist1,angle2,dist2,xpos,ypos)
+        plotBlurCannyHough(IB,BW,Tcommon1,Rcommon1,Tcommon2,Rcommon2)
     
-    return R0, C0, RH, CW
-
-# Gaussian blur
-print('Gaussian blurring...\n')
-IB = filters.gaussian(I, 15)
-
-# Canny edge detection
-print('Canny edge detection...\n')
-BW = feature.canny(IB, sigma=10, low_threshold=0.9, high_threshold=0.7, use_quantiles=True)
-
-# Hough transform
-print('Calculating Hough transform...\n')
-H, T, R = transform.hough_line(BW,np.linspace(5*np.pi/12,7*np.pi/12,120*100))
-
-# extract most common lines in the image from the Hough transform
-print('Finding most common lines from Hough transform...\n')
-num_lines = 7
-_, Tcommon, Rcommon = transform.hough_line_peaks(H,T,R,num_peaks=num_lines)
-
-# draw Houghlines on a canvas with same dimensions as the input image
-print('Filling canvas with most the %d most common lines...\n' % num_lines)
-canvas = np.zeros((I.shape[0], I.shape[1]))
-for r0, c0, r1, c1 in zip(*Houghlines2Imageboundary(Tcommon,Rcommon,I)):
-    # use line_aa() instead of line() because the rows and columns are rounded floats
-    rr, cc = draw.line(r0, c0, r1, c1)
-    # update canvas
-    canvas[rr, cc] += 1
-
-# make lines thicker with gaussian blur
-canvasblur = filters.gaussian(canvas, 5)
-
-# extract pipette tip
-ypos, xpos = np.where(canvasblur == np.max(canvasblur))
-xpos = xpos[-1]; ypos = ypos[-1]
-print('Pipette tip located at pixel coordinate: (x,y) = (%d,%d).' % (xpos,ypos))
-
-#######################################################################
+    return xpos, ypos
+    
 
 
-# make figures
-if plotflag:
-    f1, axs1 = plt.subplots(1,3)
-    axs1[0].imshow(I, cmap='gray'); axs1[0].axis('image'); axs1[0].axis('off')
-    axs1[1].imshow(canvasblur, cmap='gray'); axs1[1].axis('image'); axs1[1].axis('off')
-    axs1[2].imshow(I, cmap='gray'); axs1[2].axis('image'); axs1[2].axis('off')
-    axs1[2].scatter(x=xpos, y=ypos, c='r', s=30)
-    axs1[2].annotate('(%d,%d)' % (xpos,ypos), (xpos+30, ypos-15), c='r')
+if __name__ == '__main__':
+    detectPipettetip(I, upper_angle, lower_angle, diameter, plotflag=True)
     
-    f2, axs2 = plt.subplots(1,3)
-    axs2[0].imshow(IB, cmap='gray', aspect = 'auto'); axs2[0].axis('image'); axs2[0].axis('off')
-    axs2[1].imshow(BW, cmap='jet', aspect = 'auto'); axs2[1].axis('image'); axs2[1].axis('off')
-    axs2[2].imshow(np.log(1+H), extent=[np.rad2deg(T[0]), np.rad2deg(T[-1]), R[-1], R[0]], aspect='auto')
-    axs2[2].set_xlabel(r'$\theta$ (radians)')
-    axs2[2].set_ylabel(r'$\rho$ (pixels)')
-    
-    for _, angle, dist in zip(*transform.hough_line_peaks(H,T,R,num_peaks=num_lines)):
-        (x0, y0) = dist*np.array([np.cos(angle), np.sin(angle)])
-        axs1[0].axline((x0 ,y0), slope=np.tan(angle + np.pi/2))
-        axs2[2].scatter(x=[angle*180/np.pi], y=[dist], c='r', s=20)
-    
-    plt.show()
