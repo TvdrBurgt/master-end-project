86f7dc8c556015100a687da2171e54448da48621
TvdrBurgt <t.h.j.vanderburgt@gmail.com>
2021-06-01 14:22:50 +0200

    Integrated live and snap threads into one, other files are irrelevant and deleted now.

--- Threading test scripts/example_GUI.py
+++ Threading test scripts/example_GUI.py
@@ -8,17 +8,16 @@ Created on Mon Apr 12 10:37:51 2021
 import sys
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QGroupBox
 import pyqtgraph.exporters
 import pyqtgraph as pg
 
-from example_backend import AutomaticPatcher
+from example_backend_v2 import CameraThread, AutoPatchThread
 
 
 class PatchClampUI(QWidget):
-    def __init__(self, camera_handle=None, motor_handle=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
         # =====================================================================
         # ---------------------------- Start of GUI ---------------------------
@@ -28,33 +27,40 @@ class PatchClampUI(QWidget):
 
         # ---------------------------- Snapshot view --------------------------
         snapshotContainer = QGroupBox()
-        snapshotContainer.setMinimumSize(600, 600)
+        snapshotContainer.setMinimumSize(1200, 600)
         snapshotLayout = QGridLayout()
 
+        # Display to project live camera view
+        self.liveWidget = pg.ImageView()
+        self.canvaslive = self.liveWidget.getImageItem()
+        self.canvaslive.setAutoDownsample(True)
+        
+        self.liveWidget.ui.roiBtn.hide()
+        self.liveWidget.ui.menuBtn.hide()
+        self.liveWidget.ui.histogram.hide()
+        snapshotLayout.addWidget(self.liveWidget, 0, 0, 1, 2)
+        
         # Display to project snapshots
         self.snapshotWidget = pg.ImageView()
-        self.canvas = self.snapshotWidget.getImageItem()
-        self.canvas.setAutoDownsample(True)
-
+        self.canvassnap = self.snapshotWidget.getImageItem()
+        self.canvassnap.setAutoDownsample(True)
+        
         self.snapshotWidget.ui.roiBtn.hide()
         self.snapshotWidget.ui.menuBtn.hide()
         self.snapshotWidget.ui.histogram.hide()
-        snapshotLayout.addWidget(self.snapshotWidget, 0, 0, 1, 3)
-
-        # Button for automatic focussing a pipette
-        request_autofocus_button = QPushButton("Autofocus pipette")
-        # request_autofocus_button.clicked.connect(self.autofocus)
-        snapshotLayout.addWidget(request_autofocus_button, 1, 0, 1, 1)
+        snapshotLayout.addWidget(self.snapshotWidget, 0, 2, 1, 2)
+        
+        # Button for starting camera acquisition. This button is a property of
+        # the container because we need its checkable state in a function.
+        self.request_autofocus_button = QPushButton("Pause live")
+        self.request_autofocus_button.setCheckable(True)
+        self.request_autofocus_button.clicked.connect(self.toggle_live)
+        snapshotLayout.addWidget(self.request_autofocus_button, 1, 0, 1, 2)
 
         # Button for making a snapshot
         request_camera_image_button = QPushButton("Snap image")
-        request_camera_image_button.clicked.connect(self.snap_shot)
-        snapshotLayout.addWidget(request_camera_image_button, 1, 1, 1, 1)
-
-        # Button for detecting pipette tip
-        request_pipette_coordinates_button = QPushButton("Detect pipette tip")
-        # request_pipette_coordinates_button.clicked.connect(self.localize_pipette)
-        snapshotLayout.addWidget(request_pipette_coordinates_button, 1, 2, 1, 1)
+        request_camera_image_button.clicked.connect(self.request_snap)
+        snapshotLayout.addWidget(request_camera_image_button, 1, 2, 1, 2)
 
         snapshotContainer.setLayout(snapshotLayout)
 
@@ -67,37 +73,41 @@ class PatchClampUI(QWidget):
         # =====================================================================
         # ---------------------------- End of GUI -----------------------------
         # =====================================================================
-
-        # Initiate backend
-        self.autopatch_instance = AutomaticPatcher()
-
-        # Move backend to a thread
-        self.thread = QThread()
-        self.autopatch_instance.moveToThread(self.thread)
-
-        # Connect our GUI display to the newimage slot
-        self.autopatch_instance.newimage.connect(self.update_graph)
-
-        # Run backend when thread is started
-        self.thread.started.connect(self.autopatch_instance.run)
+        
+        # Initiate backend threads
+        self.camerathread = CameraThread()
+        # self.autopatchthread = AutoPatchThread(self.camerathread)
+        
+        # Connect our GUI display to the signal slots
+        self.camerathread.snapsignal.connect(self.update_canvassnap)
+        self.camerathread.livesignal.connect(self.update_canvaslive)
 
         # Start thread
-        self.thread.start()
-
-    def snap_shot(self):
+        self.camerathread.start()
+        # self.autopatchthread.start()
+        
+    def toggle_live(self):
+        # Request to pause or continue emitting frames from the camera thread
+        if self.request_autofocus_button.isChecked():
+            self.camerathread.stop_canvasupdates()
+        else:
+            self.camerathread.start_canvasupdates()
+    
+    def request_snap(self):
         # Request a snapshot from the camera thread
-        self.autopatch_instance.snap_image()
-
-    def update_graph(self, image):
-        # Display the newly snapped image
-        self.canvas.setImage(image)
+        self.camerathread.snap()
+        
+    def update_canvaslive(self, image):
+        # Update the live canvas with every new camera frame
+        self.canvaslive.setImage(image)
+        
+    def update_canvassnap(self, image):
+        # Update the snap canvas with the newest camera frame
+        self.canvassnap.setImage(image)
 
     def closeEvent(self, event):
-        """ Interupts widget processes
-        
-        On closing the application we have to make sure that the console
-        gets freed.
-        """
+        self.camerathread.__del__()
+        # self.autopatchthread.__del__()
         QtWidgets.QApplication.quit()
         event.accept()
 
@@ -112,5 +122,6 @@ if __name__ == "__main__":
         mainwin = PatchClampUI()
         mainwin.show()
         app.exec_()
+        
 
     run_app()
--- Threading test scripts/example_backend.py
+++ Threading test scripts/example_backend.py
@@ -5,100 +5,85 @@ Created on Thu Apr 15 11:12:26 2021
 @author: tvdrb
 """
 
-import time
 import numpy as np
 
 from copy import copy
 
-from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtCore import pyqtSignal, QThread
 
 
-class AutomaticPatcher(QObject):
-    newimage = pyqtSignal(np.ndarray)
-
-    def __init__(
-        self,
-        camera_handle=None,
-        motor_handle=None,
-        micromanipulator_handle=None,
-        *args,
-        **kwargs
-    ):
-        """
+class CameraThread(QThread):
+    snapsignal = pyqtSignal(np.ndarray)
+    livesignal = pyqtSignal(np.ndarray)
+    
+    def __init__(self):
+        self.isrunning = True
+        self.livecanvas = True
+        self.exposure_time = 20
+        self.get_frame = np.zeros((2048,2048))
         
-        Parameters
-        ----------
-        camera_handle : TYPE, optional
-            Handle to control Hamamatsu camera. The default is None.
-        motor_handle : TYPE, optional
-            Handle to control PI motor. The default is None.
-        patchstar_handle : TYPE, optional
-            Handle to control Scientifica PatchStar. Default is None.
-
-        Returns
-        -------
-        None.
+        super().__init__()
+        self.moveToThread(self)
+        self.started.connect(self.acquire)
         
-        """
-        QObject.__init__(self, *args, **kwargs)
+    def __del__(self):
+        self.isrunning = False
+        self.quit()
+        self.wait()
+    
+    def acquire(self):
+        print("data acquisition started")
+        while self.isrunning:
+            # Mutex lock here?
+            self.get_frame = np.random.rand(2048, 2048)
+            QThread.msleep(self.exposure_time)
+            if self.livecanvas:
+                self.livesignal.emit(self.get_frame)
+            else:
+                pass
+        print("data acquisition stopped")
+    
+    def start_canvasupdates(self):
+        self.livecanvas = True
         
-        self.lastview = np.random.rand(2048, 2048)
+    def stop_canvasupdates(self):
+        self.livecanvas = False
+        
+    def snap(self):
+        # Mutex lock here?
+        last_view = copy(self.get_frame)
+        print("snap!")
+        self.snapsignal.emit(last_view)
+        
+        return last_view
+        
 
-        # Static parameter settings
-        self.exposure_time = 0.02  # camera exposure time (in seconds)
 
-        # Variable paramater settings that methods will update
-        self.manipulator_position_absolute = [0, 0, 0]  # x,y,z in micrometers
-        self.manipulator_position_relative = [0, 0, 0]  # x,y,z in micrometers
-        self.objective_position = 0  # z in micrometers/10
+class AutoPatchThread(QThread):
 
-        """
-        #====================== Connect hardware devices ======================
-        # """
-        # # Create a camera instance if the handle is not provided.
-        # print('Connecting camera...')
-        # if camera_handle == None:
-        #     self.hamamatsu_cam_instance = CamActuator()
-        #     self.hamamatsu_cam_instance.initializeCamera()
-        # else:
-        #     self.hamamatsu_cam_instance = camera_handle
-
-        # # Create an objective motor instance if the handle is not provided.
-        # print('Connecting objective motor...')
-        # if motor_handle == None:
-        #     self.pi_device_instance = PIMotor()
-        # else:
-        #     self.pi_device_instance = motor_handle
-
-        # # Create a micromanipulator instance if the handle is not provided.
-        # print('Connecting micromanipulator...')
-        # if micromanipulator_handle == None:
-        #     self.micromanipulator_instance = ScientificaPatchStar()
-        # else:
-        #     self.micromanipulator_instance = micromanipulator_handle
-
-        """
-        #==================== Get hardware device settings ====================
-        """
-        # self.manipulator_position_absolute = self.micromanipulator_instance.getPos()
-        # self.objective_position = self.pi_device_instance.GetCurrentPos()
+    def __init__(self, camera_handle):
+        self.camera = camera_handle
+        self.isrunning = True
+        
+        super().__init__()
+        self.moveToThread(self)
+        self.started.connect(self.get_cameraFOV)
+        
+    def __del__(self):
+        self.isrunning = False
+        self.quit()
+        self.wait()
 
     def run(self):
-        print("data acquisition started")
-        while True:
-            self.lastview = np.random.rand(2048, 2048)
-            time.sleep(self.exposure_time)
+        # Do something here
+        pass
 
-    def snap_image(self):
-        # Copy last camera view for display or image analysis
-        snapped_image = copy(self.lastview)
-        print("snap!")
-
-        # Emit the newly snapped image for display in widget
-        self.newimage.emit(snapped_image)
-
-        return snapped_image
+    def get_cameraFOV(self):
+        while self.isrunning:
+            QThread.msleep(1000)
+            snapped_image = self.camera.snap()
+        print('autopatcher stopped')
 
 
 if __name__ == "__main__":
-    instance = AutomaticPatcher()
+    instance = AutoPatchThread()
