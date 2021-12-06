2ca3f8c5e89befe458773a33c6a782a936a3b262
TvdrBurgt <t.h.j.vanderburgt@gmail.com>
2021-12-06 12:43:02 +0100

    Extended the serial command list by adding commands to control the valves and pumps individually. Bugfixes where pressure pulses did not use the right valve states. Added a sleep function, and a wake-up function so the pressure controller can stay plugged in.

--- Pressure controller/pressure_controller_v2/pressure_controller_v2.ino
+++ Pressure controller/pressure_controller_v2/pressure_controller_v2.ino
@@ -7,6 +7,8 @@
  * spec sheets, the valves have a response time of 6ms or less. We choose a response 
  * time of 10ms to be at the safe side. All pressures are given in mBar. The motor- 
  * driver for the pump needs a PWM input between 15-50kHz, 20kHz is recommended.
+ * For the command list, look at process_serial_request() to find all possible input
+ * commands. Feel free to add 
  * 
  * External libraries 'pwm_lib' and 'tc_lib' were downloaded here:
  * https://github.com/antodom/pwm_lib
@@ -43,8 +45,11 @@ using namespace arduino_due::pwm_lib;
 String command;
 String command1;
 String command2;
-bool flag;
+bool flag_once;
+bool flag_continuous;
+bool flag_spike;
 int target_pressure;
+int pulse_magnitude;
 int pumps_PWM;
 int PS1_output;
 int PS2_output;
@@ -55,19 +60,27 @@ float P2;
 float dP;
 unsigned long previous_LCD, current;
 const long refresh_time = 1000/LCD_freq;
+const float PV1_offset = -1.89;
+const float PV2_offset = -1.89;
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // pin PC2 mapped to pin 34 on the DUE, this object uses PWM channel L0
-pwm<pwm_pin::PWML0_PC2> pwm_pin34;
+pwm<pwm_pin::PWML0_PC2> pwm_pin34;  // PUMP_PRESSURE_PWM
 // pin PC4 mapped to pin 36 on the DUE, this object uses PWM channel L1
-pwm<pwm_pin::PWML1_PC4> pwm_pin36;
+pwm<pwm_pin::PWML1_PC4> pwm_pin36;  // PUMP_VACUUM_PWM
 
 
 void setup() {
   // Arduino DUE baud rate
   Serial.begin(9600);
+
+  // initialize the LCD and set timer for refresh rate
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0); lcd.print("Setting up...");
+  previous_LCD = millis();
   
   // Arduino DUE can read 12 bits instead of default 10
   analogReadResolution(12);
@@ -83,45 +96,43 @@ void setup() {
   digitalWrite(VALVE1, LOW); digitalWrite(LED1, HIGH);
   digitalWrite(VALVE2, LOW); digitalWrite(LED2, HIGH);
   
-  // initialize the LCD
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(0, 0); lcd.print("Setting up...");
-  
   // Starting PWM signals
+  pumps_PWM = 400;
   pwm_pin34.start(PUMP_PERIOD, PUMP_PRESSURE_PWM);
   pwm_pin36.start(PUMP_PERIOD, PUMP_VACUUM_PWM);
-//  change_duty(pwm_pin34, PUMP_PRESSURE_PWM, PUMP_PERIOD);
-//  change_duty(pwm_pin36, PUMP_VACUUM_PWM, PUMP_PERIOD);
 
   // Open VALVE1 briefly to let pressure off tubing
   digitalWrite(VALVE1, HIGH); digitalWrite(LED1, LOW);
-  delay(500);
+  delay(250);
   digitalWrite(VALVE1, LOW); digitalWrite(LED1, HIGH);
-  delay(500);
-
+  delay(250);
+  
   // Calculate pressure sensor offset that we correct for later
-  for(int i=0; i<1000; i++){
+  PS1_output = 0;
+  PS2_output = 0;
+  for(int i=0; i<10000; i++){
     PS1_output = PS1_output - analogRead(PressureSensor1);
     PS2_output = PS2_output - analogRead(PressureSensor2);
   }
-  PS1_offset = voltage2pressure(PS1_output/1000);
-  PS2_offset = voltage2pressure(PS2_output/1000);
+  PS1_offset = voltage2pressure(PS1_output/10000);
+  PS2_offset = voltage2pressure(PS2_output/10000);
   Serial.print("PS1 offset: "); Serial.print(PS1_offset); Serial.println(" mBar");
   Serial.print("PS2 offset: "); Serial.print(PS2_offset); Serial.println(" mBar");
-
-  // Udate LCD and start timer for refresh rate
-  previous_LCD = millis();
-  lcd.setCursor(0,1); lcd.print("Setup complete");
-  lcd.clear();
   
   target_pressure = 0;
-  flag = true;
-  
+  flag_once = true;
+  flag_continuous = true;
+  flag_spike = false;
+  lcd.clear();
 }
 
 
 void loop() {
+  
+  // Process serial requests:
+  if (Serial.available()) {
+    process_serial_request();
+  }
   
   // Read out pressure sensors, average, and convert to pressure
   read_pressure_sensors();
@@ -129,14 +140,9 @@ void loop() {
   // Update LCD at a rate of LCD_freq
   update_lcd();
 
-  // Process serial requests:
-  if (Serial.available()) {
-    process_serial_request();
-  }
-
   //// To do only once
-  if (flag) {
-    flag = false;
+  if (flag_once) {
+    flag_once = false;
     
     // target_pressure: ATM
     if (target_pressure == 0){
@@ -150,7 +156,7 @@ void loop() {
     else if (target_pressure < 0) {
       if (target_pressure - P2 > MARGIN) {
         // bring pressure up to target_pressure
-        digitalWrite(VALVE1, HIGH); digitalWrite(LED1, LOW);
+        digitalWrite(VALVE1, HIGH); digitalWrite(LED1, LOW); 
         delay(10);
         Serial.print("Compensation upward: ");
         Serial.println(P2);
@@ -196,8 +202,43 @@ void loop() {
     } else if (abs(target_pressure) > 150) {
       pumps_PWM = 1800;
     }
-    
   }
+
+  //// To do when spike
+  if (flag_spike) {
+    flag_spike = false;
+    
+    // close the valve after the pressure tank
+    digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
+    delay(10);
+    
+    // built up pressure in the pressure tank
+    if (pulse_magnitude > 0) {
+      Serial.println("Positive pulse");
+      digitalWrite(VALVE1, HIGH); digitalWrite(LED1, LOW);
+      change_duty(pwm_pin34, 1900, PUMP_PERIOD);
+      change_duty(pwm_pin36, 0, PUMP_PERIOD);
+      while (P2 < pulse_magnitude) {
+        read_pressure_sensors();
+      }
+      change_duty(pwm_pin34, 0, PUMP_PERIOD);
+    } else if (pulse_magnitude < 0) {
+      Serial.println("Negative pulse");
+      digitalWrite(VALVE1, LOW); digitalWrite(LED1, HIGH);
+      change_duty(pwm_pin34, 0, PUMP_PERIOD);
+      change_duty(pwm_pin36, 1900, PUMP_PERIOD);
+      while (P2 > pulse_magnitude) {
+        read_pressure_sensors();
+      }
+      change_duty(pwm_pin36, 0, PUMP_PERIOD);
+    }
+    
+    // open valve after the pressure tank briefly
+    digitalWrite(VALVE2, LOW); digitalWrite(LED2, HIGH);
+    delay(50);
+    digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
+  }
+  
   
   //// To do continuous
   // Set vacuum pump dutycycle
@@ -229,8 +270,20 @@ void read_pressure_sensors() {
     PS1_output = PS1_output - analogRead(PressureSensor1);
     PS2_output = PS2_output - analogRead(PressureSensor2);
   }
+  
+  // correct for pressure sensor offset
   P1 = voltage2pressure(PS1_output/10) - PS1_offset;
   P2 = voltage2pressure(PS2_output/10) - PS2_offset;
+
+  // correct for offset from valves
+  if (digitalRead(VALVE1)) {
+    P1 = P1 - PV1_offset;
+    P2 = P2 - PV1_offset;
+  }
+  if (digitalRead(VALVE2)) {
+    P1 = P1 - PV2_offset;
+    P2 = P2 - PV2_offset;
+  }
   
   // Send out pressur readout over serial port every X ms
   Serial.println((String)"PS "+P1+" "+P2);
@@ -255,44 +308,54 @@ void process_serial_request() {
     command2 = getValue(command, ' ', 1);
     Serial.print(command1+" "); Serial.println(command2);
     if (command1 == "P") {
+      // set pressure
       target_pressure = command2.toFloat();
-      flag = true;
-    } else if(command1 = "S") {
-      apply_pressure_pulse(command2.toFloat());
+      flag_once = true;
+      flag_continuous = true;
+      flag_spike = false;
+    } else if(command1 == "S") {
+      // make pressure spike
+      pulse_magnitude = command2.toFloat();
       target_pressure = 0;
-      flag = true;
+      flag_once = false;
+      flag_continuous = false;
+      flag_spike = true;
+    } else if (command1 == "IDLE") {
+      // set device idle
+      set_idle();
+    } else if (command1 == "W8KE") {
+      // wake-up device
+      setup();
+    } else if (command1 == "V2H") {
+      digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
+    } else if (command1 == "V2L") {
+      digitalWrite(VALVE2, LOW); digitalWrite(LED2, HIGH);
+    } else if (command1 == "V1H") {
+      digitalWrite(VALVE1, HIGH); digitalWrite(LED1, LOW);
+    } else if (command1 == "V1L") {
+      digitalWrite(VALVE1, LOW); digitalWrite(LED1, HIGH);
+    } else if (command1 == "PPON") {
+      change_duty(pwm_pin34, 1900, PUMP_PERIOD);
+    } else if (command1 == "PPOFF") {
+      change_duty(pwm_pin34, 0, PUMP_PERIOD);
+    } else if (command1 == "PVON") {
+      change_duty(pwm_pin36, 1900, PUMP_PERIOD);
+    } else if (command1 == "PVOFF") {
+      change_duty(pwm_pin36, 0, PUMP_PERIOD);
     }
 }
 
 
-void apply_pressure_pulse(float pulse) {
-  // close the valve after the pressure tank
-  digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
+void set_idle() {
+  lcd.clear(); lcd.print("Going in standby");
+  change_duty(pwm_pin34, 0, PUMP_PERIOD);
+  change_duty(pwm_pin36, 0, PUMP_PERIOD);
+  digitalWrite(VALVE1, LOW); digitalWrite(LED1, HIGH);
+  digitalWrite(VALVE2, LOW); digitalWrite(LED2, HIGH);
   delay(10);
+  lcd.noBacklight();
 
-  // built up pressure in the pressure tank
-  if (pulse > 0) {
-    Serial.println("Positive pulse");
-    digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
-    change_duty(pwm_pin36, 0, PUMP_PERIOD);
-    change_duty(pwm_pin34, 1900, PUMP_PERIOD);
-    while (P1 < pulse) {
-      read_pressure_sensors();
-    }
-    change_duty(pwm_pin34, 0, PUMP_PERIOD);
-  } else if (pulse < 0) {
-    Serial.println("Negative pulse");
-    digitalWrite(VALVE2, LOW); digitalWrite(LED2, HIGH);
-    change_duty(pwm_pin34, 0, PUMP_PERIOD);
-    change_duty(pwm_pin36, 1900, PUMP_PERIOD);
-    while (P1 > pulse) {
-      read_pressure_sensors();
-    }
-    change_duty(pwm_pin36, 0, PUMP_PERIOD);
+  while (!Serial.available()) {
   }
-
-  // open valve after the pressure tank briefly
-  digitalWrite(VALVE2, HIGH); digitalWrite(LED2, LOW);
-  delay(100);
 }
  
